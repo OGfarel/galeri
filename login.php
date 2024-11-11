25b6a5e17839f45ec5fc41bf5830cc6493984ee1
OGfarel <farelthegoat@gmail.com>
2024-11-11 11:09:39 +0700

    membuat website galeri foto

---
+++ login.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Login</title>
+</head>
+<body>
+    <h1>Halaman Login</h1>
+    <form action="proses-login.php" method="post">
+        <table>
+            <tr>
+                <td>Username</td>
+                <td><input type="text" name="username"></td>
+            </tr>
+            <tr>
+                <td>Password</td>
+                <td><input type="password" name="password"></td>
+            </tr>
+            
+            <tr>
+                <td></td>
+                <td><input type="submit" value="Login"></td>
+            </tr>
+
+            <tr>
+            <td><a href="register.php">Register</a></td>
+</tr>
+
+        </table>
+    </form>
+</body>
+</html> 
